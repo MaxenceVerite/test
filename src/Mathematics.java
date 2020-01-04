787f6c43ec21909d9546bd13ad9d798d8a139c8e
MaxenceVérité <p.verite.maxence@gmail.com>
2020-01-04 19:29:11 +0100

    initial commit

---
+++ src/Mathematics.java
@@ -0,0 +1,30 @@
+
+public final class Mathematics {
+	
+	
+	
+	public final static double add(double a, double b){ 
+		
+		return a+b;
+		
+	}
+	
+	
+	public final static double sub(double a, double b) { 
+		
+		return a-b;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+}
